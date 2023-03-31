77cdc453012731a61919ecd9766ec19d78fc5969
Claude Mokbel <claudemokbel@gmail.com>
2023-03-31 08:19:54 -0700

    create close button

--- vue-trello/components/CloseButton.vue
+++ vue-trello/components/CloseButton.vue
@@ -1,6 +1,6 @@
 <template>
     <span
-        class="btn bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white px-2 py-1 border border-red-500 hover:border-transparent rounded-lg">
-        x
+        class="btn bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white px-2 border border-red-500 hover:border-transparent rounded-full">
+        X
     </span>
 </template>
